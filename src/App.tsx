c8869fa8f54e3d943dce5894056e12f064c147ed
Marcos <marcosdevelopment.business@gmail.com>
2024-06-15 17:46:42 -0300

    feat: show when the folder is empty

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import { FolderPlus } from 'lucide-react'
 import { Button } from './components/ui/button'
 import { useFileSystem } from './hooks/use-file-system'
 import { CurrentPath } from './components/current-path'
-import { ItemInfo, Kind } from './hooks/use-file-system/types'
-import { FolderDisplay } from './components/folder-display'
-import { FileDisplay } from './components/file-display'
+import { Ls } from './components/ls'
 
 export function App() {
   const fs = useFileSystem()
-
-  const items = fs.ls()
 
   return (
     <main className='w-full mx-auto max-w-6xl'>
@@ -22,35 +18,8 @@ export function App() {
           </Button>
         </div>
       </header>
-      <table className='block p-5'>
-        <thead>
-          <tr className='px-2 block'>
-            <th className='w-10'></th>
-            <th className='text-sm text-zinc-600 w-40 px-5' align='left'>
-              Name
-            </th>
-            <th className='text-sm text-zinc-600 w-52 px-5' align='left'>
-              Created at
-            </th>
-            <th className='text-sm text-zinc-600 w-52 px-5' align='left'>
-              Updated at
-            </th>
-          </tr>
-        </thead>
-        <tbody className='block mt-5'>
-          {items.map(item => {
-            if (item.kind === Kind.Dir) {
-              return (
-                <FolderDisplay key={item.name} info={item as ItemInfo<Kind.Dir>} onClick={fs.cd} />
-              )
-            }
 
-            if (item.kind === Kind.File) {
-              return <FileDisplay key={item.name} info={item as ItemInfo<Kind.File>} />
-            }
-          })}
-        </tbody>
-      </table>
+      <Ls fs={fs} />
     </main>
   )
 }
---
+++ src/components/ls/index.tsx
@@ -0,0 +1,46 @@
+import { ItemInfo, Kind } from '~/hooks/use-file-system/types'
+import { FolderDisplay } from '~/components/folder-display'
+import { FileDisplay } from '~/components/file-display'
+import { UseFileSystem } from '~/hooks/use-file-system'
+
+type Props = {
+  fs: UseFileSystem
+}
+
+export function Ls({ fs }: Props) {
+  const items = fs.ls()
+
+  if (items.length === 0) return <p className='p-5 font-mono text-sm text-zinc-600'>empty</p>
+
+  return (
+    <table className='block p-5'>
+      <thead>
+        <tr className='px-2 block'>
+          <th className='w-10'></th>
+          <th className='text-sm text-zinc-600 w-40 px-5' align='left'>
+            name
+          </th>
+          <th className='text-sm text-zinc-600 w-52 px-5' align='left'>
+            created at
+          </th>
+          <th className='text-sm text-zinc-600 w-52 px-5' align='left'>
+            updated at
+          </th>
+        </tr>
+      </thead>
+      <tbody className='block mt-5'>
+        {items.map(item => {
+          if (item.kind === Kind.Dir) {
+            return (
+              <FolderDisplay key={item.name} info={item as ItemInfo<Kind.Dir>} onClick={fs.cd} />
+            )
+          }
+
+          if (item.kind === Kind.File) {
+            return <FileDisplay key={item.name} info={item as ItemInfo<Kind.File>} />
+          }
+        })}
+      </tbody>
+    </table>
+  )
+}
