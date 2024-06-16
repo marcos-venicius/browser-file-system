93de304ba0bcefe8d7f9632035c2ea950bde8079
Marcos <marcosdevelopment.business@gmail.com>
2024-06-16 08:55:02 -0300

    feat: show folder actions

--- src/components/folder-display/index.tsx
+++ src/components/folder-display/index.tsx
@@ -1,8 +1,11 @@
 import 'dayjs/locale/pt-br'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
-import { Folder } from 'lucide-react'
+import { Folder, Trash } from 'lucide-react'
 import { ItemInfo, Kind } from '~/types'
+import { Button } from '../ui/button'
+import { useState } from 'react'
+import { cn } from '~/lib/utils'
 
 dayjs.extend(relativeTime)
 dayjs.locale('pt-br')
@@ -13,10 +16,15 @@ type Props = {
 }
 
 export function FolderDisplay({ info, onClick }: Props) {
+  const [hoveringActions, setHoveringActions] = useState(false)
+
   return (
     <tr
-      className='mb-1 block hover:bg-zinc-100 p-2 rounded transition-colors cursor-pointer'
-      onClick={onClick.bind(null, info.location)}>
+      className={cn(
+        'group relative mb-1 block p-2 rounded transition-colors cursor-pointer',
+        !hoveringActions && 'hover:bg-zinc-100'
+      )}
+      onClick={!hoveringActions ? onClick.bind(null, info.location) : undefined}>
       <td className='w-10'>
         <Folder size={15} className='text-zinc-600' />
       </td>
@@ -27,6 +35,15 @@ export function FolderDisplay({ info, onClick }: Props) {
       </td>
       <td className='w-52 px-5 text-sm font-mono text-zinc-800'>{dayjs().to(info.createdAt)}</td>
       <td className='w-52 px-5 text-sm font-mono text-zinc-800'>{dayjs().to(info.updatedAt)}</td>
+
+      <td
+        className='absolute h-full right-0 top-1/2 -translate-y-1/2 flex items-center justify-end gap-1 transition-opacity opacity-0 group-hover:opacity-100'
+        onMouseEnter={setHoveringActions.bind(null, true)}
+        onMouseLeave={setHoveringActions.bind(null, false)}>
+        <Button variant='link' className='h-8 w-10 p-0 flex items-center justify-center'>
+          <Trash size={16} className='text-red-500' />
+        </Button>
+      </td>
     </tr>
   )
 }
